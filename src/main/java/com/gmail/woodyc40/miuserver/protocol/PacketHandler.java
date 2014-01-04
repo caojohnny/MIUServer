52ed48537d8e3fe939c0e609ed1027ed9425014e
AgentTroll <woodyc40@gmail.com>
2014-01-03 21:06:41 -0800

    Update PacketHandler.java

--- src/main/java/com/gmail/woodyc40/miuserver/protocol/PacketHandler.java
+++ src/main/java/com/gmail/woodyc40/miuserver/protocol/PacketHandler.java
@@ -1,22 +1,22 @@
 package com.gmail.woodyc40.miuserver.protocol;
 
 import com.gmail.woodyc40.miuserver.Logger;
-import com.gmail.woodyc40.miuserver.frame.ClientObjectStream;
 import com.gmail.woodyc40.miuserver.frame.threadsafe.ServerThread;
 import com.gmail.woodyc40.miuserver.protocol.event.EventHandler;
 
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class PacketHandler {
     ServerThread thread;
 
     public static void sendPacket(ServerThread thread, Packet packet) {
         try {
-            ClientObjectStream stream = thread.getClient().getClientOutput();
+            ObjectOutputStream stream = thread.getClient().getClientOutput();
             
-            stream.getClientOutput().writeObject(packet);
-            stream.getClientOutput().flush();
-            stream.getClientOutput().reset();
+            stream.writeObject(packet);
+            stream.flush();
+            stream.reset();
         } catch (IOException e) {
             Logger.getInstance().logError("Packet could not be written to the stream", e);
         }
