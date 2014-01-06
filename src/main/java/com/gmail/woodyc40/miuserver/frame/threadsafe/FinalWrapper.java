3295f6418ce5a05a2ea260bf7927adf01db073b6
AgentTroll <woodyc40@gmail.com>
2014-01-06 06:31:49 -0800

    Update FinalWrapper.java

--- src/main/java/com/gmail/woodyc40/miuserver/frame/threadsafe/FinalWrapper.java
+++ src/main/java/com/gmail/woodyc40/miuserver/frame/threadsafe/FinalWrapper.java
@@ -1,6 +1,6 @@
 package com.gmail.woodyc40.miuserver.frame.threadsafe;
 
-public static class FinalWrapper<T> {
+public class FinalWrapper<T> {
     public final T value;
     
     public FinalWrapper(T value) {
