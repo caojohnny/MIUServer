e4a35482d051be5efa06c97204de1011f28192cb
AgentTroll <woodyc40@gmail.com>
2014-01-07 23:23:55 -0800

    Update PacketHandler.java

--- src/main/java/com/gmail/woodyc40/miuserver/protocol/PacketHandler.java
+++ src/main/java/com/gmail/woodyc40/miuserver/protocol/PacketHandler.java
@@ -3,6 +3,7 @@ package com.gmail.woodyc40.miuserver.protocol;
 import com.gmail.woodyc40.miuserver.Logger;
 import com.gmail.woodyc40.miuserver.api.entity.Player;
 import com.gmail.woodyc40.miuserver.frame.AdvancedServer;
+import com.gmail.woodyc40.miuserver.frame.threadsafe.FinalWrapper;
 import com.gmail.woodyc40.miuserver.frame.threadsafe.ServerThread;
 import com.gmail.woodyc40.miuserver.protocol.event.EventHandler;
 
@@ -12,7 +13,23 @@ import java.io.ObjectOutputStream;
 public class PacketHandler {
     ServerThread thread;
 
-    public static void sendPacket(ServerThread thread, Packet packet) {
+    private static FinalWrapper<PacketHandler> handler;
+
+    public static Logger getInstance() {
+        FinalWrapper<PacketHandler> wrapper = logger;
+
+        if (wrapper == null) {
+            synchronized(PacketHandler.class) {
+                if (handler == null) {
+                    handler = new FinalWrapper<>(new Logger());
+                }
+                wrapper = handler;
+            }
+        }
+        return wrapper.value;
+    }
+
+    private void writePacket(ServerThread thread, Packet packet) {
         try {
             ObjectOutputStream stream = thread.getClient().getClientOutput();
             
@@ -22,8 +39,11 @@ public class PacketHandler {
         } catch (IOException e) {
             Logger.getInstance().logError("Packet could not be written to the stream", e);
         }
+    }
+
+    public static void sendPacket(ServerThread thread, Packet packet) {
+        writePacket(thread, packet);
         EventHandler.getInstance().handleEvent(packet);
-        handlePacket(packet);
     }
 
     public static void handlePacket(Packet packet) {
